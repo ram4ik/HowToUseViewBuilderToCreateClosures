f1440062b6e402851da1906f5941540dd6459b52
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-09-23 21:32:17 +0300

    Initial Commit

---
+++ HowToUseViewBuilderToCreateClosures/HowToUseViewBuilderToCreateClosuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  HowToUseViewBuilderToCreateClosuresApp.swift
+//  HowToUseViewBuilderToCreateClosures
+//
+//  Created by ramil on 23.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct HowToUseViewBuilderToCreateClosuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
